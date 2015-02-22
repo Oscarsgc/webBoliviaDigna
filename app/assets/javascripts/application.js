f9e8178a7a28c9346ca3d3cfb5caecfb6634dd09
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2015-02-21 22:52:40 -0400

    mierdina

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
+
 //= require jquery
+//= require bootstrap
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
