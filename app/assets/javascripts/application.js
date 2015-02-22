94018aeafc0955608fd72cc6471ca856f294cfd2
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2015-02-21 23:05:29 -0400

    Local

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 //= require jquery
-//= require bootstrap
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
 
